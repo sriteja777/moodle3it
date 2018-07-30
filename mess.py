86cc363a7114e072fbfc776f1e22b4762588c83c
sriteja777 <sritejakittu777@gmail.com>
2018-07-31 04:14:39 +0530

    You can view your mess registration with this file

---
+++ mess.py
@@ -0,0 +1,15 @@
+import all
+import requests
+import  bs4
+login_page, sp = all.login()
+index = 'https://mess.iiit.ac.in/mess/web/index.php'
+home = 'https://mess.iiit.ac.in/mess/web/student_home.php'
+pg = requests.get(index, cookies=login_page.cookies)
+soup = bs4.BeautifulSoup(pg.text, 'lxml')
+k=1
+for i in soup.find('table').find_all('td')[7:13]:
+    if k%2:
+        print(i.text, ':', end='')
+    else:
+        print(i.text)
+    k = k + 1
