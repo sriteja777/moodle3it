1c3a7b36f45d9586c4484bc26dccb5a71351534f
Sriteja Sugoor <sritejakittu777@gmail.com>
2018-12-16 18:33:51 +0530

    resolved imports

--- mess.py
+++ mess.py
@@ -1,15 +1,17 @@
-import all
+import bs4
 import requests
-import  bs4
+
+import all
+
 login_page = all.login()
 index = 'https://mess.iiit.ac.in/mess/web/index.php'
 home = 'https://mess.iiit.ac.in/mess/web/student_home.php'
 pg = requests.get(index, cookies=login_page.cookies)
 soup = bs4.BeautifulSoup(pg.text, 'lxml')
-k=1
+counter = 1
 for i in soup.find('table').find_all('td')[7:13]:
-    if k%2:
+    if counter % 2:
         print(i.text, ':', end='')
     else:
         print(i.text)
-    k = k + 1
+    counter = counter + 1
